539fc7e2f47c9ba76fbe2f0879f1096542f34510
Nishant <ncp1435@gmail.com>
2023-12-08 19:35:42 +0530

    19_fetch api data in sever component

--- 02_rendering/src/app/serverproduct/page.js
+++ 02_rendering/src/app/serverproduct/page.js
@@ -1,10 +1,35 @@
+import { fetchServerResponse } from 'next/dist/client/components/router-reducer/fetch-server-response';
+import { Festive } from 'next/font/google';
 
-const page = () => {
-	return <div>
-<h3>Serve Product page</h3>
+async function fetchserverData() {
+	console.log('fetch data');
 
+	try {
+		let data = await fetch('https://fakestoreapi.com/products');
+		data = await data.json();
+		return data;
+	} catch (error) {
+		console.log('error in data');
+	}
+}
 
-    </div>;
+const page = async () => {
+	let productlist = await fetchserverData();
+	console.log(productlist);
+
+	return (
+		<div>
+			<h3>Server Product paged</h3>
+			<div>
+				{productlist.map((item) => (
+					<div>
+						<h2>{item.id}</h2>
+						<h2>{item.title}</h2>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default page;
